73b9682d252da33f79cb7c229fc8f3d656be6be3
curso13 <mr.diegocoro@gmail.com~>
2024-09-02 10:07:29 +0200

    cors y lista de cursos

--- backend/gestionCursos/src/main/java/com/gestionCursos/gestionCursos/Controller/CursoController.java
+++ backend/gestionCursos/src/main/java/com/gestionCursos/gestionCursos/Controller/CursoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,8 @@ import com.gestionCursos.gestionCursos.Service.CursoService;
 
 @RestController
 @RequestMapping("/cursos")
+@CrossOrigin(origins = "http://localhost:5173") 
+
 public class CursoController {
 
     @Autowired
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,85 +1,38 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div id="app">
+    <h1>Lista de Cursos</h1>
+    <ul>
+      <li v-for="curso in cursos" :key="curso.id">{{ curso.nombre }}</li>
+    </ul>
+  </div>
 </template>
 
+<script>
+import { ref, onMounted } from 'vue';
+import axios from './axios'; // Importa tu instancia de Axios
+
+export default {
+  setup() {
+    const cursos = ref([]);
+
+    const fetchCursos = async () => {
+      try {
+        const response = await axios.get('/api/cursos/all');
+        cursos.value = response.data;
+      } catch (error) {
+        console.error('Error fetching cursos:', error);
+      }
+    };
+
+    onMounted(() => {
+      fetchCursos();
+    });
+
+    return { cursos };
+  }
+};
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+/* Agrega tus estilos aquí */
 </style>
